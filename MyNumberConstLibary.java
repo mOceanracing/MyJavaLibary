bc7fdf047cb6b93f89b605aaf35656b48cef6ce2
mOceanracing <marc.kissling@outlook.com>
2017-02-23 10:42:52 +0100

    Update MyNumberConstLibary.java

--- MyNumberConstLibary.java
+++ MyNumberConstLibary.java
@@ -1,59 +1,58 @@
 
 public class NumberConst {
-  
-// Strings  
-Public final static String STRING_ONE = "1"; 
-Public final static String STRING_TWO = "2"; 
-Public final static String STRING_THREE = "3"; 
-Public final static String STRING_FOUR = "4"; 
-Public final static String STRING_FIVE = "5"; 
-Public final static String STRING_SIX = "6"; 
-Public final static String STRING_SEVEN = "7"; 
-Public final static String STRING_EIGHT = "8"; 
-Public final static String STRING_NINE = "9"; 
-Public final static String STRING_TEN = "10"; 
-Public final static String STRING_ONE_HUNDERT = "100"; 
-Public final static String STRING_ONE_THOUSEND = "1000"; 
+  // Strings  
+  public final static String STRING_ONE = "1"; 
+  public final static String STRING_TWO = "2"; 
+  public final static String STRING_THREE = "3"; 
+  public final static String STRING_FOUR = "4"; 
+  public final static String STRING_FIVE = "5"; 
+  public final static String STRING_SIX = "6"; 
+  public final static String STRING_SEVEN = "7"; 
+  public final static String STRING_EIGHT = "8"; 
+  public final static String STRING_NINE = "9"; 
+  public final static String STRING_TEN = "10"; 
+  public final static String STRING_ONE_HUNDERT = "100"; 
+  public final static String STRING_ONE_THOUSEND = "1000"; 
 
-// Long 
-Public final static long LONG_ONE = 1L; 
-Public final static long LONG_TWO = 2L;
-Public final static long LONG_THREE = 3L; 
-Public final static long LONG_FOUR = 4L; 
-Public final static long LONG_FIVE = 5L; 
-Public final static long LONG_SIX = 6L; 
-Public final static long LONG_SEVEN = 7L; 
-Public final static long LONG_EIGHT = 8L; 
-Public final static long LONG_NINE = 9L; 
-Public final static long LONG_TEN = 10L; 
-Public final static long LONG_ONE_HUNDERT = 100L; 
-Public final static long LONG_ONE_THOUSEND = 1000L; 
- 
-// Int 
-Public final static int INT_ONE = 1 
-Public final static int INT_TWO = 2; 
-Public final static int INT_THREE = 3; 
-Public final static int INT_FOUR = 4; 
-Public final static int INT_FIVE = 5; 
-Public final static int INT_SIX = 6; 
-Public final static int INT_SEVEN = 7; 
-Public final static int INT_EIGHT = 8; 
-Public final static int INT_NINE = 9; 
-Public final static int INT_TEN = 10; 
-Public final static int INT_ONE_HUNDERT = 100; 
-Public final static int INT_ONE_THOUSEND = 1000; 
+  // Long 
+  public final static long LONG_ONE = 1L; 
+  public final static long LONG_TWO = 2L;
+  public final static long LONG_THREE = 3L; 
+  public final static long LONG_FOUR = 4L; 
+  public final static long LONG_FIVE = 5L; 
+  public final static long LONG_SIX = 6L; 
+  public final static long LONG_SEVEN = 7L; 
+  public final static long LONG_EIGHT = 8L; 
+  public final static long LONG_NINE = 9L; 
+  public final static long LONG_TEN = 10L; 
+  public final static long LONG_ONE_HUNDERT = 100L; 
+  public final static long LONG_ONE_THOUSEND = 1000L; 
 
-//Double 
-Public final static double DOUBLE_ONE = 1.0; 
-Public final static double DOUBLE_TWO = 2.0; 
-Public final static double DOUBLE_THREE = 3.0; 
-Public final static double DOUBLE_FOUR = 4.0; 
-Public final static double DOUBLE_FIVE = 5.0; 
-Public final static double DOUBLE_SIX = 6.0; 
-Public final static double DOUBLE_SEVEN = 7.0; 
-Public final static double DOUBLE_EIGHT = 8.0; 
-Public final static double DOUBLE_NINE = 9.0; 
-Public final static double DOUBLE_TEN = 10.0; 
-Public final static double DOUBLE_ONE_HUNDERT = 100.0; 
-Public final static double DOUBLE_ONE_THOUSEND = 1000.0;
+  // Int 
+  public final static int INT_ONE = 1 
+  public final static int INT_TWO = 2; 
+  public final static int INT_THREE = 3; 
+  public final static int INT_FOUR = 4; 
+  public final static int INT_FIVE = 5; 
+  public final static int INT_SIX = 6; 
+  public final static int INT_SEVEN = 7; 
+  public final static int INT_EIGHT = 8; 
+  public final static int INT_NINE = 9; 
+  public final static int INT_TEN = 10; 
+  public final static int INT_ONE_HUNDERT = 100; 
+  public final static int INT_ONE_THOUSEND = 1000; 
+
+  //Double 
+  public final static double DOUBLE_ONE = 1.0; 
+  public final static double DOUBLE_TWO = 2.0; 
+  public final static double DOUBLE_THREE = 3.0; 
+  public final static double DOUBLE_FOUR = 4.0; 
+  public final static double DOUBLE_FIVE = 5.0; 
+  public final static double DOUBLE_SIX = 6.0; 
+  public final static double DOUBLE_SEVEN = 7.0; 
+  public final static double DOUBLE_EIGHT = 8.0; 
+  public final static double DOUBLE_NINE = 9.0; 
+  public final static double DOUBLE_TEN = 10.0; 
+  public final static double DOUBLE_ONE_HUNDERT = 100.0; 
+  public final static double DOUBLE_ONE_THOUSEND = 1000.0;
 }
