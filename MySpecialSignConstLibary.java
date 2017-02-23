a437d1cc099c94e0c68cc74bb2e2b95c923552ee
mOceanracing <marc.kissling@outlook.com>
2017-02-23 10:44:02 +0100

    Update MySpecialSignConstLibary.java

--- MySpecialSignConstLibary.java
+++ MySpecialSignConstLibary.java
@@ -1,34 +1,34 @@
 
 public interface MySpecialSignConstLibary {
-  
-Public final static String EMPTY_STRING=""; 
-Public final static String SPACE=""; 
-Public final static String SLASH="/"; 
-Public final static String STRICH="-"; 
-Public final static String NEWLINE="\n"; 
-Public final static String DOT="."; 
-Public final static String EXCLAMATION_MARK="!"; 
-Public final static String NEWLINE_HTML="<br>"; 
-Public final static String PIPE="|"; 
-Public final static String TAB="\t"; 
-Public final static String COMMA=","; 
-Public final static String UNDERLINE="_"; 
-Public final static String COLON=":"; 
-Public final static String SEMICOLON=";"; 
-Public final static String STAR="*"; 
-Public final static String DOUBLEQUOTE="\""; 
-Public final static String SINGLEQUOTE="'";
-Public final static String BIGGER_OPERATOR="<"; 
-Public final static String SMALER_OPERATOR=">"; 
-Public final static String BRACKET_ROUND_RIGHT=")"; 
-Public final static String BRACKET_ROUND_LEFT="("; 
-Public final static String BRACKET_SQUARE_RIGHT="]"; 
-Public final static String BRACKET_SQUARE_LEFT="["; 
-Public final static String BRACKET_CURLY_RIGHT="}"; 
-Public final static String BRACKET_CURLY_LEFT="{"; 
-Public final static String GLEICH="="; 
-Public final static String NEWLINE_CRLF="\r\n"; 
-Public final static String NUMBER_SIGN="#"; 
-Public final static String PERCENT_SIGN="%"; 
-Public final static String DOLLAR_SIGN="$";
+
+  public final static String EMPTY_STRING=""; 
+  public final static String SPACE=""; 
+  public final static String SLASH="/"; 
+  public final static String STRICH="-"; 
+  public final static String NEWLINE="\n"; 
+  public final static String DOT="."; 
+  public final static String EXCLAMATION_MARK="!"; 
+  public final static String NEWLINE_HTML="<br>"; 
+  public final static String PIPE="|"; 
+  public final static String TAB="\t"; 
+  public final static String COMMA=","; 
+  public final static String UNDERLINE="_"; 
+  public final static String COLON=":"; 
+  public final static String SEMICOLON=";"; 
+  public final static String STAR="*"; 
+  public final static String DOUBLEQUOTE="\""; 
+  public final static String SINGLEQUOTE="'";
+  public final static String BIGGER_OPERATOR="<"; 
+  public final static String SMALER_OPERATOR=">"; 
+  public final static String BRACKET_ROUND_RIGHT=")"; 
+  public final static String BRACKET_ROUND_LEFT="("; 
+  public final static String BRACKET_SQUARE_RIGHT="]"; 
+  public final static String BRACKET_SQUARE_LEFT="["; 
+  public final static String BRACKET_CURLY_RIGHT="}"; 
+  public final static String BRACKET_CURLY_LEFT="{"; 
+  public final static String GLEICH "="; 
+  public final static String NEWLINE_CRLF="\r\n"; 
+  public final static String NUMBER_SIGN="#"; 
+  public final static String PERCENT_SIGN="%"; 
+  public final static String DOLLAR_SIGN="$";
 }
