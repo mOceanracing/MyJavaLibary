c02057208a747291c9eeceb5137bb3c99e94ba48
mOceanracing <marc.kissling@outlook.com>
2017-02-23 10:35:12 +0100

    Update HTMLConst.java

--- HTMLConst.java
+++ HTMLConst.java
@@ -1,4 +1,28 @@
 
-public interface HTMLConst {
+public class HTMLConst {
+  public static final String HTML_TAG_START="<HTML>"; 
+  public static final String HTML_TAG_END="</HTML>"; 
+  public static final String BODY_TAG_START="<BODY>"; 
+  public static final String BODY_TAG_END="</BODY>"; 
 
+  public static final String HTML_BOLD_START="<b>"; 
+  public static final String HTML_BOLD_END="</b>"; 
+  public static final String HTML_ITALIC_START="<i>"; 
+  public static final String HTML_ITALIC_END="</i>"; 
+  public static final String HTML_BREAK="<br/>"; 
+  public static final String HTML_HORIZONTAL_RULE="<hr/>"; 
+  
+  //Überschriften h1 - h6 
+  public static final String HTML_H1_START="<h1>"; 
+  public static final String HTML_H1_END="<h1>"; 
+  public static final String HTML_H1_START="<h2>"; 
+  public static final String HTML_H1_END="</h2>"; 
+  public static final String HTML_H1_START="<h3>"; 
+  public static final String HTML_H1_END="</h3>"; 
+  public static final String HTML_H1_START="<h4>"; 
+  public static final String HTML_H1_END="</h4>"; 
+  public static final String HTML_H1_START="<h5>"; 
+  public static final String HTML_H1_END="</h5>"; 
+  public static final String HTML_H1_START="<h6>"; 
+  public static final String HTML_H1_END="</h6>";
 }
