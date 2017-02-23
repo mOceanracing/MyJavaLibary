968d02aa5f7ac40ef64a930252c71f2dadf6848a
mOceanracing <marc.kissling@outlook.com>
2017-02-23 10:27:01 +0100

    Update and rename NumberConst.java to MyNumberConstLibary.java

---
+++ MyNumberConstLibary.java
@@ -0,0 +1,59 @@
+
+public class NumberConst {
+  
+// Strings  
+Public final static String STRING_ONE = "1"; 
+Public final static String STRING_TWO = "2"; 
+Public final static String STRING_THREE = "3"; 
+Public final static String STRING_FOUR = "4"; 
+Public final static String STRING_FIVE = "5"; 
+Public final static String STRING_SIX = "6"; 
+Public final static String STRING_SEVEN = "7"; 
+Public final static String STRING_EIGHT = "8"; 
+Public final static String STRING_NINE = "9"; 
+Public final static String STRING_TEN = "10"; 
+Public final static String STRING_ONE_HUNDERT = "100"; 
+Public final static String STRING_ONE_THOUSEND = "1000"; 
+
+// Long 
+Public final static long LONG_ONE = 1L; 
+Public final static long LONG_TWO = 2L;
+Public final static long LONG_THREE = 3L; 
+Public final static long LONG_FOUR = 4L; 
+Public final static long LONG_FIVE = 5L; 
+Public final static long LONG_SIX = 6L; 
+Public final static long LONG_SEVEN = 7L; 
+Public final static long LONG_EIGHT = 8L; 
+Public final static long LONG_NINE = 9L; 
+Public final static long LONG_TEN = 10L; 
+Public final static long LONG_ONE_HUNDERT = 100L; 
+Public final static long LONG_ONE_THOUSEND = 1000L; 
+ 
+// Int 
+Public final static int INT_ONE = 1 
+Public final static int INT_TWO = 2; 
+Public final static int INT_THREE = 3; 
+Public final static int INT_FOUR = 4; 
+Public final static int INT_FIVE = 5; 
+Public final static int INT_SIX = 6; 
+Public final static int INT_SEVEN = 7; 
+Public final static int INT_EIGHT = 8; 
+Public final static int INT_NINE = 9; 
+Public final static int INT_TEN = 10; 
+Public final static int INT_ONE_HUNDERT = 100; 
+Public final static int INT_ONE_THOUSEND = 1000; 
+
+//Double 
+Public final static double DOUBLE_ONE = 1.0; 
+Public final static double DOUBLE_TWO = 2.0; 
+Public final static double DOUBLE_THREE = 3.0; 
+Public final static double DOUBLE_FOUR = 4.0; 
+Public final static double DOUBLE_FIVE = 5.0; 
+Public final static double DOUBLE_SIX = 6.0; 
+Public final static double DOUBLE_SEVEN = 7.0; 
+Public final static double DOUBLE_EIGHT = 8.0; 
+Public final static double DOUBLE_NINE = 9.0; 
+Public final static double DOUBLE_TEN = 10.0; 
+Public final static double DOUBLE_ONE_HUNDERT = 100.0; 
+Public final static double DOUBLE_ONE_THOUSEND = 1000.0;
+}
