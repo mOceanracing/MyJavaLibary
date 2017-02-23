a1e2473b4739c22a09643d12fe0370741495ef85
mOceanracing <marc.kissling@outlook.com>
2017-02-23 10:32:20 +0100

    Update MyFileEndingLibary.java

--- MyFileEndingLibary.java
+++ MyFileEndingLibary.java
@@ -1,21 +1,12 @@
 Public class MyFileEndingLibary{
-Public final static String FILE_EXTENSION_XML=".xml"; 
-
-Public final static String FILE_EXTENSION_PDF=".pdf"; 
-
-Public final static String FILE_EXTENSION_TXT=".pdf"; 
-
-Public final static String FILE_EXTENSION_WORD=".docx"; 
-
-Public final static String FILE_EXTENSION_EXCEL=".xlsx"; 
-
-Public final static String FILE_EXTENSION_VISIO=".vsdx"; 
-
-Public final static String FILE_EXTENSION_POWERPOINT=".pptx"; 
-
-Public final static String FILE_EXTENSION_HTML=".html"; 
-
-Public final static String FILE_EXTENSION_CSS=".css"; 
-
-Public final static String FILE_EXTENSION_ZIP=".zip"
+  Public final static String FILE_EXTENSION_XML=".xml"; 
+  Public final static String FILE_EXTENSION_PDF=".pdf"; 
+  Public final static String FILE_EXTENSION_TXT=".pdf"; 
+  Public final static String FILE_EXTENSION_WORD=".docx"; 
+  Public final static String FILE_EXTENSION_EXCEL=".xlsx"; 
+  Public final static String FILE_EXTENSION_VISIO=".vsdx"; 
+  Public final static String FILE_EXTENSION_POWERPOINT=".pptx"; 
+  Public final static String FILE_EXTENSION_HTML=".html"; 
+  Public final static String FILE_EXTENSION_CSS=".css"; 
+  Public final static String FILE_EXTENSION_ZIP=".zip"
 }
