57959215aa3a524377f4d322db34232593036499
mOceanracing <marc.kissling@outlook.com>
2017-02-23 10:30:08 +0100

    Update and rename SpecSignConst.java to MySpecialSignConstLibary.java.java

---
+++ MySpecialSignConstLibary.java.java
@@ -0,0 +1,34 @@
+
+public interface MySpecialSignConstLibary {
+  
+Public final static String EMPTY_STRING=""; 
+Public final static String SPACE=""; 
+Public final static String SLASH="/"; 
+Public final static String STRICH="-"; 
+Public final static String NEWLINE="\n"; 
+Public final static String DOT="."; 
+Public final static String EXCLAMATION_MARK="!"; 
+Public final static String NEWLINE_HTML="<br>"; 
+Public final static String PIPE="|"; 
+Public final static String TAB="\t"; 
+Public final static String COMMA=","; 
+Public final static String UNDERLINE="_"; 
+Public final static String COLON=":"; 
+Public final static String SEMICOLON=";"; 
+Public final static String STAR="*"; 
+Public final static String DOUBLEQUOTE="\""; 
+Public final static String SINGLEQUOTE="'";
+Public final static String BIGGER_OPERATOR="<"; 
+Public final static String SMALER_OPERATOR=">"; 
+Public final static String BRACKET_ROUND_RIGHT=")"; 
+Public final static String BRACKET_ROUND_LEFT="("; 
+Public final static String BRACKET_SQUARE_RIGHT="]"; 
+Public final static String BRACKET_SQUARE_LEFT="["; 
+Public final static String BRACKET_CURLY_RIGHT="}"; 
+Public final static String BRACKET_CURLY_LEFT="{"; 
+Public final static String GLEICH="="; 
+Public final static String NEWLINE_CRLF="\r\n"; 
+Public final static String NUMBER_SIGN="#"; 
+Public final static String PERCENT_SIGN="%"; 
+Public final static String DOLLAR_SIGN="$";
+}
