771bde3fac76156a6d28e93c144c5214088324aa
mOceanracing <marc.kissling@outlook.com>
2017-02-23 11:20:06 +0100

    Update MyFXDialogLibary.java

--- MyFXDialogLibary.java
+++ MyFXDialogLibary.java
@@ -1,3 +1,19 @@
 public class MyFXDialogLibary{
+	public static void showInfoDialog(String titelText, String contentText){
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle(titelText);
+		alert.setHeaderText(null);
+		alert.setContentText(contentText);
+	
+		alert.showAndWait();
+	}	
+	
+	public static void showWarningDialog(String titelText, String headerText, String contentText){
+		Alert alert = new Alert(AlertType.WARNING);
+		alert.setTitle(titelText);
+		alert.setHeaderText(headerText);
+		alert.setContentText(contentText);
 
+		alert.showAndWait();
+	}	
 }
